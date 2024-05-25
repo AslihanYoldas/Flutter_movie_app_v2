cba2d4f09fccfb6e99913917650e25ca53b68999
aslıhan yoldaş <aslihanyoldas24@gmail.com>
2024-05-25 20:50:30 +0300

    Project updated

--- lib/widgets/video.dart
+++ lib/widgets/video.dart
@@ -1,8 +1,6 @@
-
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-
 import '../utils/modifed_text.dart';
 
 class VideoWidget extends StatefulWidget {
